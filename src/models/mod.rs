5f5530d6e8287c2c4fddf1513dc431c4381b7a82
E <edgeworth.dev@gmail.com>
2021-01-07 23:44:53 +0900

    Updates

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -6,20 +6,6 @@ pub mod qmk;
 pub mod us;
 
 pub trait Model {
-    type M: Model;
-
     fn valid(&mut self, pev: PhysEv) -> bool;
-    fn event(&mut self, pev: PhysEv) -> Transition<Self::M>;
-}
-
-#[derive(Debug, Clone, Eq, PartialEq, Hash)]
-pub struct Transition<M: Model> {
-    pub s: M,
-    pub kev: Vec<KeyEv>,
-}
-
-impl<M: Model> Transition<M> {
-    pub fn new(s: M, kev: &[KeyEv]) -> Self {
-        Self { s, kev: kev.to_vec() }
-    }
+    fn event(&mut self, pev: PhysEv) -> Vec<KeyEv>;
 }
