bc3746d81bbcf64e3b9e1b578b684b3c9caf35dd
E <edgeworth.dev@gmail.com>
2021-01-12 01:21:15 +0900

    Optimize.

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -2,7 +2,7 @@ use crate::constants::Constants;
 use crate::models::count_map::CountMap;
 use crate::types::{PhysEv, KC};
 
-mod count_map;
+pub mod count_map;
 pub mod key_automata;
 pub mod layout;
 pub mod qmk;
@@ -11,5 +11,11 @@ mod tests;
 pub mod us;
 
 pub trait Model {
+    // Takes a phys event and returns a vector of counts of each keycode. Multiple countmaps may be
+    // returned if releasing a key confirms some pending events that were held back. If it returns
+    // none, it means the physical event was not valid. The model will not be valid to use after it
+    // returns None here.
     fn event(&mut self, pev: PhysEv, cnst: &Constants) -> Option<Vec<CountMap<KC>>>;
+    // Return the countmap of keys currently pressed.
+    fn kc_counts(&self) -> &CountMap<KC>;
 }
--- src/path.rs
+++ src/path.rs
@@ -1,34 +1,35 @@
 use crate::constants::Constants;
 use crate::ga::util::{combine_cost, combine_fitness};
 use crate::layout_eval::LayoutCfg;
+use crate::models::count_map::CountMap;
 use crate::models::layout::Layout;
 use crate::models::qmk::QmkModel;
 use crate::models::us::USModel;
 use crate::models::Model;
-use crate::types::PhysEv;
+use crate::types::{KCSetExt, PhysEv, KC};
 use derive_more::Display;
 use ordered_float::OrderedFloat;
 use priority_queue::PriorityQueue;
 use std::collections::HashSet;
+use std::usize;
 
 #[derive(Debug, Clone, Ord, PartialOrd, Eq, PartialEq, Hash, Display)]
-#[display(fmt = "Node, corpus idx {}:\n  qmk: {}\n  us: {}", idx, qmk, us)]
+#[display(fmt = "Node, corpus idx {}:\n  qmk: {}", idx, qmk)]
 struct Node<'a> {
     pub qmk: QmkModel<'a>, // Currently have this keyboard state.
-    pub us: USModel,
-    pub idx: usize, // Processed this much of the corpus.
+    pub idx: usize, // Processed this much of the corpus (transformed to countmaps of keycodes).
 }
 
 impl<'a> Node<'a> {
     pub fn new(layout: &'a Layout) -> Self {
-        Self { qmk: QmkModel::new(layout), us: USModel::new(), idx: 0 }
+        Self { qmk: QmkModel::new(layout), idx: 0 }
     }
 }
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct PathFinder<'a> {
     layout_cfg: &'a LayoutCfg,
-    corpus: &'a [PhysEv],
+    events: Vec<CountMap<KC>>,
     cnst: &'a Constants,
     l: &'a Layout,
 }
@@ -40,7 +41,13 @@ impl<'a> PathFinder<'a> {
         cnst: &'a Constants,
         l: &'a Layout,
     ) -> Self {
-        Self { layout_cfg, corpus, cnst, l }
+        let mut events = Vec::new();
+        let mut us = USModel::new();
+        for &pev in corpus.iter() {
+            // If we get a stray release which causes US model to fail, ignore and skip it.
+            events.extend(us.event(pev, cnst).unwrap_or_default());
+        }
+        Self { layout_cfg, events, cnst, l }
     }
 
     fn phys_cost(&self, pev: PhysEv) -> f64 {
@@ -50,44 +57,55 @@ impl<'a> PathFinder<'a> {
     // Check that |ev| could be produced from Node by consuming corpus operations.
     fn try_event<'b>(&self, mut n: Node<'b>, pev: PhysEv) -> Option<Node<'b>> {
         let mut events_qmk = n.qmk.event(pev, &self.cnst)?;
-        while !events_qmk.is_empty() && n.idx < self.corpus.len() {
-            // If we get a stray release which causes US model to fail, ignore and skip it.
-            if let Some(mut events_us) = n.us.event(self.corpus[n.idx], &self.cnst) {
-                while !events_us.is_empty() && !events_qmk.is_empty() {
-                    if events_us[0] != events_qmk[0] {
-                        return None;
-                    }
-                    events_us.remove(0);
-                    events_qmk.remove(0);
-                }
+        for ev in events_qmk.into_iter() {
+            if ev != self.events[n.idx] {
+                return None;
             }
             n.idx += 1;
         }
-        if events_qmk.is_empty() {
-            Some(n)
-        } else {
-            None
-        }
+        Some(n)
     }
+
+    // fn compute_cumulative(&self) -> Vec<usize> {
+    //     let mut reg_presses = Vec::new();
+    //     let us = USModel::new();
+    //     for prev in self.events.iter() {
+    //         // Assumes that a physical key will always map to the same key event.
+    //         let reg_count = us.get_key(prev.phys).reg().len();
+    //         reg_presses.push(reg_count);
+    //     }
+    //     let c = reg_presses
+    //         .iter()
+    //         .rev()
+    //         .scan(0, |c, cnt| {
+    //             *c += cnt;
+    //             Some(*c)
+    //         })
+    //         .collect::<Vec<_>>();
+    //     let mut c: Vec<usize> = c.iter().rev().copied().collect();
+    //     c.push(0);
+    //     c
+    // }
 
     pub fn path_fitness(&self) -> u128 {
         let mut q: PriorityQueue<Node<'_>, OrderedFloat<f64>> = PriorityQueue::new();
         let mut seen: HashSet<Node<'_>> = HashSet::new();
         let mut best = (0, OrderedFloat(0.0));
-        // let mut reg_cumsum = Vec::new();
 
         let st = Node::new(self.l);
         q.push(st.clone(), OrderedFloat(0.0));
+        let mut cnt = 0;
         while let Some((n, d)) = q.pop() {
             let d = -d;
             seen.insert(n.clone());
+            cnt += 1;
 
             // println!("cost: {}, dijk: {}, seen: {}", -d, n, seen.len());
             // Look for getting furthest through corpus, then for lowest cost.
             if n.idx > best.0 || (n.idx == best.0 && d < best.1) {
                 best = (n.idx, d)
             }
-            if n.idx >= self.corpus.len() {
+            if n.idx >= self.events.len() {
                 break;
             }
             // Try pressing and releasing physical keys.
@@ -96,6 +114,16 @@ impl<'a> PathFinder<'a> {
                     let next = n.clone();
                     let pev = PhysEv::new(i, press);
                     if let Some(next) = self.try_event(next, pev) {
+                        // Assume that every press of a mod key will generate an event upon release.
+                        // TODO: this is not valid in case US model does e.g. ctrl, shift, alt and
+                        // wants to build up mods?
+
+                        // let qmk_mod_count = next.qmk.kc_counts().mods();
+                        // let us_mod_count = next.us.kc_counts().mods();
+                        // if qmk_mod_count != us_mod_count && qmk_mod_count.is_superset(&us_mod_count)
+                        // {
+                        //     continue;
+                        // }
                         if seen.contains(&next) {
                             continue;
                         }
@@ -106,24 +134,17 @@ impl<'a> PathFinder<'a> {
             }
         }
         // Typing all corpus is top priority, then cost to do so.
-        // println!(
-        //     "asdf {} {} {}, stuck on: {:?}",
-        //     best.0 as u128,
-        //     st.start_idx,
-        //     block_size as u128,
-        //     st.us.get_key(self.corpus[best.0 as usize].phys)
-        // );
+        // println!("asdf {} {}, {}", best.0 as u128, self.events.len() as u128, cnt);
 
-        let fitness = combine_fitness(0, best.0 as u128, self.corpus.len() as u128);
+        let fitness = combine_fitness(0, best.0 as u128, self.events.len() as u128);
         let fitness =
-            combine_cost(fitness, best.1.into_inner() as u128, self.corpus.len() as u128 * 1000);
+            combine_cost(fitness, best.1.into_inner() as u128, self.events.len() as u128 * 1000);
         fitness
     }
 }
 
 #[cfg(test)]
 mod tests {
-    
 
     #[test]
     fn path_fitness() {}
