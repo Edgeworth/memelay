788cf48515fe0a089c9155f627af03fd458ebd3f
E <edgeworth.dev@gmail.com>
2021-01-11 14:13:46 +0900

    Remove valid function. Improve layer switching. Add tests.

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,14 +1,15 @@
 use crate::constants::Constants;
 use crate::models::count_map::CountMap;
+use crate::prelude::*;
 use crate::types::{PhysEv, KC};
 
 mod count_map;
 pub mod key_automata;
 pub mod layout;
 pub mod qmk;
+mod tests;
 pub mod us;
 
 pub trait Model {
-    fn valid(&mut self, pev: PhysEv, cnst: &Constants) -> bool;
-    fn event(&mut self, pev: PhysEv, cnst: &Constants) -> Vec<CountMap<KC>>;
+    fn event(&mut self, pev: PhysEv, cnst: &Constants) -> Option<Vec<CountMap<KC>>>;
 }
