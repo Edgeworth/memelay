96cd9368b3bc3d7e6bbf28279cff64d767ca3b02
E <edgeworth.dev@gmail.com>
2021-01-16 15:35:11 +0900

    Update

--- benches/path.rs
+++ benches/path.rs
@@ -7,8 +7,6 @@ use designer::models::layout::Layout;
 use designer::models::us::USModel;
 use designer::path::PathFinder;
 use designer::types::KeyEv;
-use designer::Args;
-use structopt::StructOpt;
 
 struct PathFinderCfg<'a> {
     layout_cfg: &'a LayoutCfg,
@@ -22,12 +20,17 @@ fn compute_path(cfg: &PathFinderCfg<'_>) -> u128 {
 }
 
 fn path(c: &mut Criterion) {
-    let args = Args::from_args();
-    let layout_cfg = load_layout_cfg(args.cfg_path).unwrap();
-    let corpus = load_corpus(args.corpus_path).unwrap();
-    let kevs = compute_kevs(USModel::new(), &corpus, &args.cnst);
+    let layout_cfg = load_layout_cfg("data/moonlander.cfg").unwrap();
+    let corpus = load_corpus("data/bench.data").unwrap();
+    let cnst = Constants {
+        max_phys_pressed: 4,
+        max_phys_idle: 2,
+        max_mod_pressed: 1,
+        ..Default::default()
+    };
+    let kevs = compute_kevs(USModel::new(), &corpus, &cnst);
     let l = load_layout("data/bench.layout").unwrap();
-    let cfg = PathFinderCfg { layout_cfg: &layout_cfg, kevs: &kevs, cnst: &args.cnst, l: &l };
+    let cfg = PathFinderCfg { layout_cfg: &layout_cfg, kevs: &kevs, cnst: &cnst, l: &l };
     c.bench_with_input(BenchmarkId::new("path", "bench data"), &cfg, |b, cfg| {
         b.iter(|| compute_path(cfg));
     });
