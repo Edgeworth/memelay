f6078fb1f8191feeadc76e26fc9a25ef898dc6c4
Eliot Courtney <edgeworth.dev@gmail.com>
2021-01-21 20:22:03 +0900

    Update

--- ga/src/lib.rs
+++ ga/src/lib.rs
@@ -8,21 +8,29 @@
     partition_point,
     bool_to_option,
     map_first_last,
-    option_unwrap_none
+    option_unwrap_none,
+    array_windows
 )]
 
 use rand_distr::weighted_alias::AliasableWeight;
+use smallvec::SmallVec;
 
-mod prelude;
 pub mod runner;
 pub mod util;
 
 pub trait Evaluator: Send + Sync + Clone {
-    type T: Clone + Send + Sync;
-    type F: Copy + Clone + Send + Sync + Default + PartialOrd + AliasableWeight;
-    fn reproduce(&self, cfg: &Cfg, a: &Self::T, b: &Self::T) -> Self::T;
-    fn fitness(&self, cfg: &Cfg, a: &Self::T) -> Self::F;
-    fn distance(&self, cfg: &Cfg, a: &Self::T, b: &Self::T) -> f64;
+    type State: Clone + Send + Sync;
+    type Fitness: Copy + Clone + Send + Sync + Default + PartialOrd + AliasableWeight;
+
+    fn crossover(
+        &self,
+        cfg: &Cfg,
+        s1: &Self::State,
+        s2: &Self::State,
+    ) -> SmallVec<[Self::State; 2]>;
+    fn mutate(&self, cfg: &Cfg, s: &mut Self::State);
+    fn fitness(&self, cfg: &Cfg, s: &Self::State) -> Self::Fitness;
+    fn distance(&self, cfg: &Cfg, s1: &Self::State, s2: &Self::State) -> f64;
 }
 
 #[derive(Debug, Clone, PartialOrd, PartialEq)]
--- ga/src/util.rs
+++ ga/src/util.rs
@@ -1,10 +1,39 @@
-pub fn crossover_vec<T: Clone>(a: &[T], b: &[T], crosspoint: usize) -> Vec<T> {
-    let mut v = Vec::new();
-    v.extend(a[..crosspoint].iter().cloned());
-    v.extend(b[crosspoint..].iter().cloned());
-    v
+use smallvec::SmallVec;
+use std::iter::{FromIterator, Iterator};
+
+// K-point crossover.
+pub fn crossover_kpx<O: FromIterator<T::Item>, T: IntoIterator>(
+    s1: T,
+    s2: T,
+    xpoints: &[usize],
+) -> (O, O) {
+    let mut s1 = s1.into_iter();
+    let mut s2 = s2.into_iter();
+    let mut o1 = Vec::new();
+    let mut o2 = Vec::new();
+    let mut xpoints: SmallVec<[usize; 4]> = SmallVec::from_slice(xpoints);
+    xpoints.push(0);
+    xpoints.sort_unstable();
+    for [prev, cur] in xpoints.array_windows::<2>() {
+        o1.extend(s1.by_ref().take(cur - prev));
+        o2.extend(s2.by_ref().take(cur - prev));
+        std::mem::swap(&mut s1, &mut s2);
+    }
+    o1.extend(s1);
+    o2.extend(s2);
+    (o1.into_iter().collect(), o2.into_iter().collect())
 }
 
+// Uniform crossover.
+// pub fn crossover_ux
+// pub fn crossover_kpx<'a, I: Clone, O: FromIterator<I>>(
+//     mut s1: &'a [I],
+//     mut s2: &'a [I]) -> O {
+
+// }
+// Mutation:
+
+// Combining fitnesses:
 pub fn combine_fitness(cur_fitness: u128, next: u128, max_next: u128) -> u128 {
     let mut unit = 1;
     while unit < max_next {
@@ -15,4 +44,17 @@ pub fn combine_fitness(cur_fitness: u128, next: u128, max_next: u128) -> u128 {
 
 pub fn combine_cost(cur_fitness: u128, next: u128, max_next: u128) -> u128 {
     combine_fitness(cur_fitness, max_next - next, max_next)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_crossover_kpx() {
+        assert_eq!(
+            crossover_kpx::<String, _>("abcd".chars(), "wxyz".chars(), &[3]),
+            ("abcz".to_string(), "wxyd".to_string())
+        );
+    }
 }
