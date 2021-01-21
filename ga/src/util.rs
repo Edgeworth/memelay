a2c8bcf2aaff41dd3ebba7b245721c6a1eb12842
Eliot Courtney <edgeworth.dev@gmail.com>
2021-01-21 20:36:21 +0900

    Add crossover functions.

--- ga/src/util.rs
+++ ga/src/util.rs
@@ -1,3 +1,4 @@
+use rand::Rng;
 use smallvec::SmallVec;
 use std::iter::{FromIterator, Iterator};
 
@@ -26,11 +27,27 @@ pub fn crossover_kpx<O: FromIterator<T::Item>, T: IntoIterator>(
 
 // Uniform crossover.
 // pub fn crossover_ux
-// pub fn crossover_kpx<'a, I: Clone, O: FromIterator<I>>(
-//     mut s1: &'a [I],
-//     mut s2: &'a [I]) -> O {
+pub fn crossover_ux<O: FromIterator<T::Item>, T: IntoIterator, R: Rng>(
+    s1: T,
+    s2: T,
+    r: &mut R,
+) -> (O, O) {
+    let s1 = s1.into_iter();
+    let s2 = s2.into_iter();
+    let mut o1 = Vec::new();
+    let mut o2 = Vec::new();
+    for (v1, v2) in s1.zip(s2) {
+        if r.gen::<bool>() {
+            o1.push(v1);
+            o2.push(v2);
+        } else {
+            o1.push(v2);
+            o2.push(v1);
+        }
+    }
+    (o1.into_iter().collect(), o2.into_iter().collect())
+}
 
-// }
 // Mutation:
 
 // Combining fitnesses:
@@ -48,13 +65,28 @@ pub fn combine_cost(cur_fitness: u128, next: u128, max_next: u128) -> u128 {
 
 #[cfg(test)]
 mod tests {
+    use rand::rngs::mock::StepRng;
+
     use super::*;
 
     #[test]
     fn test_crossover_kpx() {
         assert_eq!(
-            crossover_kpx::<String, _>("abcd".chars(), "wxyz".chars(), &[3]),
+            crossover_kpx("abcd".chars(), "wxyz".chars(), &[3]),
             ("abcz".to_string(), "wxyd".to_string())
+        );
+        assert_eq!(
+            crossover_kpx("abcd".chars(), "wxyz".chars(), &[1, 2]),
+            ("axcd".to_string(), "wbyz".to_string())
+        );
+    }
+
+    #[test]
+    fn test_crossover_ux() {
+        let mut r = StepRng::new(1 << 31, 1 << 31);
+        assert_eq!(
+            crossover_ux("abcd".chars(), "wxyz".chars(), &mut r),
+            ("axcz".to_string(), "wbyd".to_string())
         );
     }
 }
