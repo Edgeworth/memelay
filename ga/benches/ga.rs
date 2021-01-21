aa058be136fc505d475453337ef757a721f48797
E <edgeworth.dev@gmail.com>
2021-01-21 21:32:18 +0900

    More util functions. Progress on ga bench.

--- ga/benches/ga.rs
+++ ga/benches/ga.rs
@@ -1,22 +1,38 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use ga::Evaluator;
+use criterion::{criterion_group, criterion_main, Criterion};
+use ga::runner::{Generation, Runner};
+use ga::util::{crossover_kpx_rand, replace_rand};
+use ga::{Cfg, Evaluator};
+use rand::Rng;
+use rand_distr::Alphanumeric;
+use smallvec::smallvec;
 use smallvec::SmallVec;
 
 type State = String;
 
 #[derive(Debug, Clone)]
-struct BenchEval {}
+struct BenchEval {
+    target: String,
+}
+
+impl BenchEval {
+    fn new(target: &str) -> Self {
+        Self { target: target.to_string() }
+    }
+}
 
 impl Evaluator for BenchEval {
     type State = State;
     type Fitness = f64;
 
-    fn crossover(&self, _: &ga::Cfg, _s1: &State, _s2: &State) -> SmallVec<[State; 2]> {
-        todo!()
+    fn crossover(&self, _: &ga::Cfg, s1: &State, s2: &State) -> SmallVec<[State; 2]> {
+        let mut r = rand::thread_rng();
+        let (c1, c2) = crossover_kpx_rand(s1.chars(), s2.chars(), 2, &mut r);
+        smallvec![c1, c2]
     }
 
-    fn mutate(&self, _: &ga::Cfg, _s: &mut State) {
-        todo!()
+    fn mutate(&self, _: &ga::Cfg, s: &mut State) {
+        let mut r = rand::thread_rng();
+        *s = replace_rand(s.chars(), r.sample(Alphanumeric) as char, &mut r);
     }
 
     fn fitness(&self, _: &ga::Cfg, _s: &State) -> f64 {
@@ -29,16 +45,25 @@ impl Evaluator for BenchEval {
 }
 
 #[inline]
-fn fibonacci(n: u64) -> u64 {
-    match n {
-        0 => 1,
-        1 => 1,
-        n => fibonacci(n - 1) + fibonacci(n - 2),
+fn evolve(target: &str) -> usize {
+    let mut r = rand::thread_rng();
+    let cfg = Cfg { crossover_rate: 0.3, pop_size: 100, top_prop: 0.1 };
+    let initial = (0..target.len()).map(|_| r.sample(Alphanumeric) as char).collect();
+    let gen = Generation::from_states(vec![initial]);
+    let mut runner = Runner::new(BenchEval::new(target), cfg, gen);
+    let mut runs = 0;
+    loop {
+        runs += 1;
+        let best = runner.run_iter();
+        println!("Generation: {} score: {:.3?}", runs, best.fitness);
+        if best.state == target {
+            return runs;
+        }
     }
 }
 
 fn ga(c: &mut Criterion) {
-    c.bench_function("fib 20", |b| b.iter(|| fibonacci(black_box(10))));
+    c.bench_function("hello world", |b| b.iter(|| evolve("hello world")));
 }
 
 criterion_group!(benches, ga);
