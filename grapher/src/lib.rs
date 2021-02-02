7b3a8414c4472889e7875e4d2445ff9116316b16
E <edgeworth.dev@gmail.com>
2021-02-03 08:52:44 +0900

    Fix

--- grapher/src/lib.rs
+++ grapher/src/lib.rs
@@ -13,9 +13,9 @@
     array_chunks
 )]
 
-use std::collections::HashMap;
-
 use crate::stats::sample::Sample;
+use eyre::Result;
+use std::collections::HashMap;
 
 pub mod stats;
 
@@ -23,12 +23,23 @@ pub mod stats;
 // TODO: Use ANOVA?
 #[derive(Debug, Default, Clone, PartialEq)]
 pub struct SampleGroup {
+    name: String,
     samples: HashMap<String, Sample>,
 }
 
+impl std::fmt::Display for SampleGroup {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "Group {}", self.name)?;
+        for (k, v) in self.samples.iter() {
+            writeln!(f, "  {}: {}", k, v)?;
+        }
+        Ok(())
+    }
+}
+
 impl SampleGroup {
-    pub fn new() -> Self {
-        Self { samples: HashMap::new() }
+    pub fn new(name: &str) -> Self {
+        Self { name: name.to_owned(), samples: HashMap::new() }
     }
 
     // Adds the sampled value to the Sample with name |id|.
@@ -36,13 +47,12 @@ impl SampleGroup {
         self.samples.entry(id.to_owned()).or_insert_with(Sample::new).add(v);
     }
 
-    pub fn analyse(&self) {
+    pub fn analyse(&self) -> Result<f64> {
         // TODO: assumes there are two things here
         let mut iter = self.samples.iter();
         let a = iter.next().unwrap().1;
         let b = iter.next().unwrap().1;
-        println!("a: {:?} b {:?}", a, b);
-        a.ttest(b);
+        a.ttest(b)
     }
 }
 
@@ -64,13 +74,14 @@ impl Grapher {
 
     // Adds the sampled value to the Sample with name |id| in group |group|.
     pub fn add(&mut self, group: &str, id: &str, v: f64) {
-        println!("Add {} {} {}", group, id, v);
-        self.groups.entry(group.to_owned()).or_insert_with(SampleGroup::new).add(id, v);
+        self.groups.entry(group.to_owned()).or_insert_with(|| SampleGroup::new(group)).add(id, v);
     }
 
     pub fn analyse(&self) {
-        for (_, v) in self.groups.iter() {
-            v.analyse();
+        for (k, v) in self.groups.iter() {
+            if let Ok(p) = v.analyse() {
+                println!("group {}, p {}: {}", k, p, v);
+            }
         }
     }
 }
