c353bc5a753853f299fb584e1e616ebd870f5296
E <edgeworth.dev@gmail.com>
2021-03-14 11:40:13 +0900

    Improve performance

--- ga/benches/examples.rs
+++ ga/benches/examples.rs
@@ -1,20 +1,55 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 use ga::cfg::{Cfg, Crossover, Mutation, Niching, Selection, Species, Survival};
+use ga::examples::ackley::ackley_runner;
+use ga::examples::griewank::griewank_runner;
+use ga::examples::knapsack::knapsack_runner;
 use ga::examples::rastrigin::rastrigin_runner;
+use ga::examples::target_string::target_string_runner;
 
-fn rastringin(c: &mut Criterion) {
-    c.bench_function("rastringin", |b| {
-        let cfg = Cfg::new(100)
-            .with_mutation(Mutation::Fixed(vec![0.9, 0.1]))
-            .with_crossover(Crossover::Fixed(vec![0.3, 0.7]))
-            .with_survival(Survival::TopProportion(0.25))
-            .with_selection(Selection::Sus)
-            .with_species(Species::None)
-            .with_niching(Niching::None);
-        let mut r = rastrigin_runner(2, cfg);
+fn get_cfg() -> Cfg {
+    Cfg::new(100)
+        .with_mutation(Mutation::Adaptive)
+        .with_crossover(Crossover::Adaptive)
+        .with_survival(Survival::TopProportion(0.25))
+        .with_selection(Selection::Sus)
+        .with_species(Species::None)
+        .with_niching(Niching::None)
+}
+
+fn rastrigin(c: &mut Criterion) {
+    c.bench_function("rastrigin", |b| {
+        let mut r = rastrigin_runner(2, get_cfg());
         b.iter(|| r.run_iter())
     });
 }
 
-criterion_group!(benches, rastringin);
+fn griewank(c: &mut Criterion) {
+    c.bench_function("griewank", |b| {
+        let mut r = griewank_runner(2, get_cfg());
+        b.iter(|| r.run_iter())
+    });
+}
+
+fn ackley(c: &mut Criterion) {
+    c.bench_function("ackley", |b| {
+        let mut r = ackley_runner(2, get_cfg());
+        b.iter(|| r.run_iter())
+    });
+}
+
+fn knapsack(c: &mut Criterion) {
+    c.bench_function("knapsack", |b| {
+        let mut r = knapsack_runner(get_cfg());
+        b.iter(|| r.run_iter())
+    });
+}
+
+fn target_string(c: &mut Criterion) {
+    c.bench_function("target_string", |b| {
+        let mut r = target_string_runner(get_cfg());
+        b.iter(|| r.run_iter())
+    });
+}
+
+criterion_group!(benches, rastrigin, griewank, ackley, knapsack, target_string);
 criterion_main!(benches);
