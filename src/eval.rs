bee41ff39a9a7aae9b96795d714676ae1078b464
E <edgeworth.dev@gmail.com>
2021-03-21 12:57:41 +0900

    Optimize using bigrams.

--- src/eval.rs
+++ src/eval.rs
@@ -1,9 +1,11 @@
+use std::collections::HashMap;
+
 use crate::ingest::{load_keys, load_params};
 use crate::layout::Layout;
 use crate::types::Kc;
 use crate::Args;
 use eyre::Result;
-use memega::ops::crossover::{crossover_cycle, crossover_pmx};
+use memega::ops::crossover::{crossover_cycle, crossover_order, crossover_pmx};
 use memega::ops::fitness::count_different;
 use memega::ops::mutation::{
     mutate_gen, mutate_insert, mutate_inversion, mutate_rate, mutate_scramble, mutate_swap,
@@ -40,11 +42,29 @@ impl Params {
 #[derive(Debug, Clone)]
 pub struct LayoutEval {
     pub params: Params,
-    pub keys: Vec<Kc>,
+    pub unigrams: HashMap<Kc, u32>,
+    pub bigrams: HashMap<(Kc, Kc), u32>,
     pub match_keys: Vec<Kc>,
 }
 
 impl LayoutEval {
+    fn build_unigrams(keys: &[Kc]) -> HashMap<Kc, u32> {
+        let mut m: HashMap<Kc, u32> = HashMap::new();
+        for &kc in keys.iter() {
+            *m.entry(kc).or_insert(0) += 1;
+        }
+        m
+    }
+
+    fn build_bigrams(keys: &[Kc]) -> HashMap<(Kc, Kc), u32> {
+        let mut m: HashMap<(Kc, Kc), u32> = HashMap::new();
+        for &[kc1, kc2] in keys.array_windows::<2>() {
+            *m.entry((kc1, kc2)).or_insert(0) += 1;
+        }
+        m
+    }
+
+
     pub fn from_args(args: &Args) -> Result<Self> {
         let params = load_params(&args.params_path)?;
         let keys = load_keys(&args.data_path)?;
@@ -80,13 +100,18 @@ impl LayoutEval {
             Kc::Dot,
             Kc::Slash,
         ];
-        Ok(Self { params, keys, match_keys })
+        Ok(Self {
+            params,
+            unigrams: Self::build_unigrams(&keys),
+            bigrams: Self::build_bigrams(&keys),
+            match_keys,
+        })
     }
 }
 
 impl Evaluator for LayoutEval {
     type Genome = Layout;
-    const NUM_CROSSOVER: usize = 3;
+    const NUM_CROSSOVER: usize = 4;
     const NUM_MUTATION: usize = 5;
 
     fn crossover(&self, s1: &mut Layout, s2: &mut Layout, idx: usize) {
@@ -96,6 +121,9 @@ impl Evaluator for LayoutEval {
                 crossover_pmx(&mut s1.keys, &mut s2.keys);
             }
             2 => {
+                crossover_order(&mut s1.keys, &mut s2.keys);
+            }
+            3 => {
                 crossover_cycle(&mut s1.keys, &mut s2.keys);
             }
             _ => panic!("unknown crossover strategy"),
@@ -141,45 +169,52 @@ impl Evaluator for LayoutEval {
         const RING_MID: &[f64] = &[0.1, 0.2, 0.3];
         let mut cost = 0.0;
 
-        let mut prev = Kc::None;
-        for &kc in self.keys.iter() {
-            // Finger penalties
-            if let Some(curi) = s.keys.iter().position(|&v| v == kc) {
-                cost += self.params.cost[curi];
-
-                // Bi-gram penalities
-                if let Some(previ) = s.keys.iter().position(|&v| v == prev) {
-                    // Model from https://colemakmods.github.io/mod-dh/compare.html
-                    let cfing = self.params.finger[curi];
-                    let pfing = self.params.finger[previ];
-                    let crow = self.params.row[curi];
-                    let prow = self.params.row[previ];
-                    let chand = self.params.hand[curi];
-                    let phand = self.params.hand[previ];
-                    let same_hand = chand == phand;
-                    let same_fing = same_hand && cfing == pfing;
-                    let pinky_ring =
-                        same_hand && (cfing == 3 && pfing == 2 || cfing == 2 && pfing == 3);
-                    let ring_mid =
-                        same_hand && (cfing == 2 && pfing == 1 || cfing == 1 && pfing == 2);
-                    let jump_len = (crow - prow).abs() as usize;
-
-                    if same_fing {
-                        cost += SAME_FING[jump_len];
-                    }
-                    if pinky_ring {
-                        cost += PINKY_RING[jump_len];
-                    }
-                    if ring_mid {
-                        cost += RING_MID[jump_len];
-                    }
-                }
+        // Check unigrams:
+        for (&kc, &count) in self.unigrams.iter() {
+            // Finger penalties - penalise for not being able to type characters.
+            let percost = if let Some(curi) = s.keys.iter().position(|&v| v == kc) {
+                self.params.cost[curi]
             } else {
-                cost += 100.0; // Penalise for not being able to type characters.
-            }
-            prev = kc;
+                100.0
+            };
+            cost += percost * count as f64;
         }
 
+        // Check bi-grams
+        for (&(kc1, kc2), &count) in self.bigrams.iter() {
+            // Model from https://colemakmods.github.io/mod-dh/compare.html
+            let previ = s.keys.iter().position(|&v| v == kc1);
+            let curi = s.keys.iter().position(|&v| v == kc2);
+            if previ.is_none() || curi.is_none() {
+                continue;
+            }
+            let previ = previ.unwrap();
+            let curi = curi.unwrap();
+
+            let cfing = self.params.finger[curi];
+            let pfing = self.params.finger[previ];
+            let crow = self.params.row[curi];
+            let prow = self.params.row[previ];
+            let chand = self.params.hand[curi];
+            let phand = self.params.hand[previ];
+            let same_hand = chand == phand;
+            let same_fing = same_hand && cfing == pfing;
+            let pinky_ring = same_hand && (cfing == 3 && pfing == 2 || cfing == 2 && pfing == 3);
+            let ring_mid = same_hand && (cfing == 2 && pfing == 1 || cfing == 1 && pfing == 2);
+            let jump_len = (crow - prow).abs() as usize;
+
+            let mut percost = 0.0;
+            if same_fing {
+                percost += SAME_FING[jump_len];
+            }
+            if pinky_ring {
+                percost += PINKY_RING[jump_len];
+            }
+            if ring_mid {
+                percost += RING_MID[jump_len];
+            }
+            cost += percost * count as f64;
+        }
 
         // Tie-breaking: similarity to qwerty:
         cost += count_different(&s.keys, &self.match_keys) as f64;
