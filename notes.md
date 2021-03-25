a77b527fd6a5fd3db294dcf7f5c4a1d9d92b5100
E <edgeworth.dev@gmail.com>
2021-03-25 14:41:52 +0900

    Update

--- notes.md
+++ notes.md
@@ -1,11 +1,60 @@
 Trained on filelist_dropbox:
-fitness: 0.26829814736960267
+0.5518628948449962
 z f l d v / ; , . j
 x n t s h g a e i w
 k m p r b y o c u q
 
+0.5520782503
+j . , ; / b d l f z
+y i e a g h s t n w
+q u c o x v r p m k
+
+0.5429816912
+z f c l v y u o ; q
+x n s r h g i e t k
+w m d p b / a , . j
+
+0.5438892661
+j ; o u x v c r l q
+y e a i g f t s n w
+k h , . / b p d m z
+
+0.5422561013
+q r d p v x h u ; j
+w n s t f g a i e y
+z m c l b / o , . k
+
+0.5429816912
+z f c l v y u o ; q
+x n s r h g i e t k
+w m d p b / a , . j
+
+0.5450699721
+z m d p v y u o ; q
+x n s r f g i e t k
+w b c l h / a , . j
+
+0.5520892919
+z f l d b / ; , . j
+w n t s h g a e i y
+k m p r v x o c u q
+
+
+# TODO: Seed
+# TODO: Assign species IDs, keep stable species?
+
 Trained on filelist_linux:
-Generation 19351: 0.24803662713023528
+0.5257562520421634
 z p d m v y o c u j
 k n t r h ; e s i w
 q b f l g . a x , /
+
+0.5218713015943004
+j ; o u / h l c p z
+w t e i . m r d n v
+y g , a k x s f b q
+
+0.5235426747
+z n d f v k u o ; j
+/ s t r h x i e c w
+q m p l b . a , g y
--- src/lib.rs
+++ src/lib.rs
@@ -15,7 +15,7 @@
 )]
 
 use crate::eval::LayoutEval;
-use crate::ingest::{load_layout, load_params};
+use crate::ingest::{load_layouts, load_params};
 use crate::layout::Layout;
 use eyre::Result;
 use memega::cfg::{Cfg, Crossover, Duplicates, Mutation, Niching, Species, Stagnation, Survival};
@@ -66,9 +66,9 @@ pub struct Args {
 pub fn eval_layout<P: AsRef<Path>>(p: P) -> Result<()> {
     let args = Args::from_args();
     let eval = LayoutEval::from_args(&args)?;
-    let l = load_layout(p)?;
-    let fitness = eval.fitness(&l);
-    println!("layout: {}", eval.params.format(&l));
+    let l = load_layouts(p)?;
+    let fitness = eval.fitness(&l[0]);
+    println!("layout:\n{}", eval.params.format(&l[0]));
     println!("fitness: {}", fitness);
     Ok(())
 }
@@ -76,9 +76,11 @@ pub fn eval_layout<P: AsRef<Path>>(p: P) -> Result<()> {
 pub fn layout_runner(cfg: Cfg) -> Result<Runner<CachedEvaluator<LayoutEval>>> {
     let args = Args::from_args();
     let eval = LayoutEval::from_args(&args)?;
-    let initial_keys = load_layout("data/default.layout")?.keys;
-    Ok(Runner::new(CachedEvaluator::new(eval, 1000), cfg, move || {
-        let mut keys = initial_keys.clone();
+    let initial_keys = load_layouts("data/default.layout")?;
+    let keyset = initial_keys[0].keys.clone();
+    println!("Seeded with {} layouts", initial_keys.len());
+    Ok(Runner::from_initial(CachedEvaluator::new(eval, 1000), cfg, initial_keys, move || {
+        let mut keys = keyset.clone();
         keys.shuffle(&mut rand::thread_rng());
         Layout::new(keys)
     }))
@@ -92,9 +94,9 @@ pub fn evolve(cfg: Cfg) -> Result<()> {
     for i in 0..200001 {
         let mut r = runner.run_iter()?;
         if i % 50 == 0 {
-            println!("Generation {}: {}", i + 1, r.gen.best().base_fitness);
+            println!("Generation {}: {}", i + 1, r.gen.nth(0).base_fitness);
             println!("{}", runner.summary(&mut r));
-            println!("{}", params.format(&r.gen.best().state.0));
+            println!("{}", runner.summary_sample(&mut r, 6, |v| params.format(v)));
         }
     }
 
@@ -102,16 +104,16 @@ pub fn evolve(cfg: Cfg) -> Result<()> {
 }
 
 pub fn hyper_evolve() -> Result<()> {
-    let mut builder = HyperBuilder::new(Duration::from_millis(10));
+    let mut builder = HyperBuilder::new(100, Duration::from_millis(2000));
     builder.add(1.0, &|cfg| layout_runner(cfg).unwrap());
     let mut runner = builder.build();
 
     for i in 0..10001 {
         let mut r = runner.run_iter()?;
-        println!("Generation {}: {}", i + 1, r.gen.best().base_fitness);
+        println!("Generation {}: {}", i + 1, r.gen.nth(0).base_fitness);
         if i % 10 == 0 {
             println!("{}", runner.summary(&mut r));
-            println!("{:?}", r.gen.best().state);
+            println!("{}", runner.summary_sample(&mut r, 5, |v| format!("{:?}", v)));
         }
     }
 
@@ -122,12 +124,12 @@ pub fn run() -> Result<()> {
     let args = Args::from_args();
     // Remember to update these values if add more mutation/crossover strategies.
     let cfg = Cfg::new(1000)
-        .with_mutation(Mutation::Adaptive)
-        .with_crossover(Crossover::Adaptive)
-        .with_survival(Survival::SpeciesTopProportion(0.2))
+        .with_mutation(Mutation::Fixed(vec![0.001, 0.2, 0.2, 0.2, 0.2]))
+        .with_crossover(Crossover::Fixed(vec![0.3, 0.1, 0.2, 0.7]))
+        .with_survival(Survival::SpeciesTopProportion(0.1))
         .with_species(Species::TargetNumber(100))
         .with_niching(Niching::None)
-        .with_stagnation(Stagnation::NumGenerations(100))
+        .with_stagnation(Stagnation::None)
         .with_duplicates(Duplicates::DisallowDuplicates)
         .with_par_fitness(true)
         .with_par_dist(true);
