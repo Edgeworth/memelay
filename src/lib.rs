cdd9a98f90dbec3fb56c537fe83505fad78fb938
E <edgeworth.dev@gmail.com>
2021-03-27 12:18:34 +0900

    Update

--- src/lib.rs
+++ src/lib.rs
@@ -18,7 +18,9 @@ use crate::eval::LayoutEval;
 use crate::ingest::{load_layouts, load_params};
 use crate::layout::Layout;
 use eyre::Result;
-use memega::cfg::{Cfg, Crossover, Duplicates, Mutation, Niching, Species, Stagnation, Survival};
+use memega::cfg::{
+    Cfg, Crossover, Duplicates, Mutation, Niching, Replacement, Species, Stagnation, Survival,
+};
 use memega::hyper::HyperBuilder;
 use memega::runner::Runner;
 use memega::{CachedEvaluator, Evaluator};
@@ -94,7 +96,7 @@ pub fn evolve(cfg: Cfg) -> Result<()> {
     for i in 0..200001 {
         let mut r = runner.run_iter()?;
         if i % 50 == 0 {
-            println!("Generation {}: {}", i + 1, r.gen.nth(0).base_fitness);
+            println!("Generation {}: {}", i + 1, r.nth(0).base_fitness);
             println!("{}", runner.summary(&mut r));
             println!("{}", runner.summary_sample(&mut r, 6, |v| params.format(v)));
         }
@@ -110,7 +112,7 @@ pub fn hyper_evolve() -> Result<()> {
 
     for i in 0..10001 {
         let mut r = runner.run_iter()?;
-        println!("Generation {}: {}", i + 1, r.gen.nth(0).base_fitness);
+        println!("Generation {}: {}", i + 1, r.nth(0).base_fitness);
         if i % 10 == 0 {
             println!("{}", runner.summary(&mut r));
             println!("{}", runner.summary_sample(&mut r, 5, |v| format!("{:?}", v)));
@@ -124,12 +126,15 @@ pub fn run() -> Result<()> {
     let args = Args::from_args();
     // Remember to update these values if add more mutation/crossover strategies.
     let cfg = Cfg::new(1000)
-        .with_mutation(Mutation::Fixed(vec![0.001, 0.2, 0.2, 0.2, 0.2]))
-        .with_crossover(Crossover::Fixed(vec![0.3, 0.1, 0.2, 0.7]))
+        .with_mutation(Mutation::Adaptive)
+        .with_crossover(Crossover::Adaptive)
+        // .with_mutation(Mutation::Fixed(vec![0.001, 0.2, 0.2, 0.2, 0.2]))
+        // .with_crossover(Crossover::Fixed(vec![0.3, 0.1, 0.2, 0.7]))
         .with_survival(Survival::SpeciesTopProportion(0.1))
-        .with_species(Species::TargetNumber(100))
+        .with_species(Species::TargetNumber(50))
         .with_niching(Niching::None)
-        .with_stagnation(Stagnation::None)
+        .with_stagnation(Stagnation::ContinuousAfter(100))
+        .with_replacement(Replacement::ReplaceChildren(0.5))
         .with_duplicates(Duplicates::DisallowDuplicates)
         .with_par_fitness(true)
         .with_par_dist(true);
