49199cc80193d8fdac99fdf8832ac5f88fbcc501
E <edgeworth.dev@gmail.com>
2021-03-31 19:18:06 +0900

    Trigram progress.

--- scripts/bigram_difficulty.py
+++ scripts/bigram_difficulty.py
@@ -32,32 +32,55 @@ def compare(a, b):
     if (a, b) in precmp:
         print('Using existing value for %s: %d' % (str((a, b)), precmp[(a, b)]))
         return precmp[(a, b)]
+    if (b, a) in precmp:
+        print('Using existing value for %s: %d' % (str((a, b)), -precmp[(b, a)]))
+        return -precmp[(b, a)]
     a = '%s %s %s' % a
     b = '%s %s %s' % b
-    print('Which is easier:')
-    print('a:', a)
-    print('b:', b)
-    ans = input('a / b / 0: ')
-    with open(FNAME, 'a') as f:
-        if ans == 'a':
-            f.write('%s < %s\n' % (a, b))
-            return -1
-        elif ans == 'b':
-            f.write('%s > %s\n' % (a, b))
-            return 1
-        elif ans == '0':
-            f.write('%s = %s\n' % (a, b))
-            return 0
+    while True:
+        print('Which is easier:')
+        print('a:', a)
+        print('b:', b)
+        ans = input('a / b / 0: ')
+        with open(FNAME, 'a') as f:
+            if ans == 'a':
+                f.write('%s < %s\n' % (a, b))
+                return -1
+            elif ans == 'b':
+                f.write('%s > %s\n' % (a, b))
+                return 1
+            elif ans == '0':
+                f.write('%s = %s\n' % (a, b))
+                return 0
+        print("Try again")
     raise "error"
 
-bigrams = list(itertools.product(fingers, rows, fingers))
-print(len(bigrams))
-bigrams.remove(('index', 'same', 'index'))  # Remove same fingers - hard to compare
-bigrams.remove(('middle', 'same', 'middle'))
-bigrams.remove(('ring', 'same', 'ring'))
-bigrams.remove(('pinky', 'same', 'pinky'))
-print(len(bigrams))
-random.shuffle(bigrams)
-bigrams.sort(key=functools.cmp_to_key(compare))
+def sort_bigrams():
+    bigrams = list(itertools.product(fingers, rows, fingers))
+    bigrams.remove(('index', 'same', 'index'))  # Remove same fingers - hard to compare
+    bigrams.remove(('middle', 'same', 'middle'))
+    bigrams.remove(('ring', 'same', 'ring'))
+    bigrams.remove(('pinky', 'same', 'pinky'))
+    random.seed(42)
+    random.shuffle(bigrams)
+    bigrams.sort(key=functools.cmp_to_key(compare))
 
-print('\n'.join('%s %s %s' % i for i in bigrams))
+    print('\n'.join('%s %s %s' % i for i in bigrams))
+
+
+def print_table(filename):
+    bigrams = {}
+    for same in fingers:
+        bigrams[(same, 'same', same)] = '0.0'  # default value
+    for line in open(filename).readlines():
+        a, b, c, v = line.split()
+        bigrams[(a, b, c)] = v
+
+
+    for first in fingers:
+        for second in fingers:
+            vals = '\t'.join(bigrams[(first, row, second)] for row in rows)
+            print(vals)
+        print()
+
+print_table('cfg/bigram_weights')
--- scripts/combine_grams.py
+++ scripts/combine_grams.py
@@ -1,8 +1,9 @@
 import sys
-from scripts.common import write_unigrams, write_bigrams
+from scripts.common import write_unigrams, write_bigrams, write_trigrams
 
-suffix1 = sys.argv[1]
-suffix2 = sys.argv[2]
+layer = sys.argv[1]
+suffix1 = sys.argv[2]
+suffix2 = sys.argv[3]
 
 def read_unigrams(name):
     lines = open(name).readlines()[1:]
@@ -20,6 +21,15 @@ def read_bigrams(name):
         bigrams[(ch1, ch2)] = float(val)
     return bigrams
 
+def read_trigrams(name):
+    lines = open(name).readlines()[1:]
+    trigrams = {}
+    for l in lines:
+        ch1, ch2, ch3, val = l.split(' ')
+        trigrams[(ch1, ch2, ch3)] = float(val)
+    return trigrams
+
+
 def combine_dist(a, b):
     keys = set(a.keys())
     keys = keys.union(set(b.keys()))
@@ -28,10 +38,15 @@ def combine_dist(a, b):
         output[k] = a.get(k, 0.0) * 0.5 + b.get(k, 0.0) * 0.5
     return output
 
-p1uni = read_unigrams('data/unigrams_%s.data' % suffix1)
-p2uni = read_unigrams('data/unigrams_%s.data' % suffix2)
-p1bi = read_bigrams('data/bigrams_%s.data' % suffix1)
-p2bi = read_bigrams('data/bigrams_%s.data' % suffix2)
+p1uni = read_unigrams('data/unigrams_%s_%s.data' % (suffix1, layer))
+p2uni = read_unigrams('data/unigrams_%s_%s.data' % (suffix2, layer))
+p1bi = read_bigrams('data/bigrams_%s_%s.data' % (suffix1, layer))
+p2bi = read_bigrams('data/bigrams_%s_%s.data' % (suffix2, layer))
+p1tri = read_trigrams('data/trigrams_%s_%s.data' % (suffix1, layer))
+p2tri = read_trigrams('data/bigrams_%s_%s.data' % (suffix2, layer))
 
-write_unigrams(combine_dist(p1uni, p2uni), 1.0, 'combined')
-write_bigrams(combine_dist(p1bi, p2bi), 1.0, 'combined')
+
+out_suffix = 'combined_%s' % layer
+write_unigrams(combine_dist(p1uni, p2uni), 1.0, out_suffix)
+write_bigrams(combine_dist(p1bi, p2bi), 1.0, out_suffix)
+write_trigrams(combine_dist(p1tri, p2tri), 1.0, out_suffix)
